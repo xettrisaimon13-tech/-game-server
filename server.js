6f6914982ea3ffd52fd730d7e0ac687c6db3f5b5
xettrisaimon13-tech <xettrisaimon13@gmail.com>
2026-06-14 03:49:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const { WebSocketServer } = require('ws');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Game Server Running');
+});
+
 const PORT = process.env.PORT || 8080;
-const wss = new WebSocketServer({ port: PORT });
+const wss = new WebSocketServer({ server });
 
 let players = {};
 let matchmaking_queue = [];
-const MATCH_SIZE = 10;
+const MATCH_SIZE = 2;
+
+server.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
+});
 
 wss.on('connection', (ws) => {
     let my_id = null;
@@ -18,30 +29,43 @@ wss.on('connection', (ws) => {
                 my_id = msg.player_id;
                 players[my_id] = { ws, name: msg.name, player_id: my_id };
                 break;
-
             case 'find_match':
                 if (!matchmaking_queue.includes(my_id)) {
                     matchmaking_queue.push(my_id);
                 }
                 check_match();
                 break;
-
             case 'cancel_match':
                 matchmaking_queue = matchmaking_queue.filter(id => id !== my_id);
                 break;
-
             case 'host_room':
                 ws.send(JSON.stringify({ type: 'room_created', room_code: my_id }));
                 break;
-
             case 'join_room':
                 const host = Object.values(players).find(p => p.player_id === msg.room_code);
                 if (host) {
                     ws.send(JSON.stringify({ type: 'join_success' }));
-                    host.ws.send(JSON.stringify({ type: 'player_joined_room', name: players[my_id]?.name, player_id: my_id }));
                 } else {
                     ws.send(JSON.stringify({ type: 'join_failed', reason: 'Room bhethiyena!' }));
                 }
                 break;
         }
     });
+
+    ws.on('close', () => {
+        if (my_id) {
+            delete players[my_id];
+            matchmaking_queue = matchmaking_queue.filter(id => id !== my_id);
+        }
+    });
+});
+
+function check_match() {
+    if (matchmaking_queue.length >= MATCH_SIZE) {
+        const room = matchmaking_queue.splice(0, MATCH_SIZE);
+        const room_id = Date.now().toString(36).toUpperCase();
+        room.forEach(pid => {
+            players[pid]?.ws.send(JSON.stringify({ type: 'match_found', room_id, players: room }));
+        });
+    }
+}
